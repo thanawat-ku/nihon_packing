209b473173db3d5507ba5f9403d8688a6d83e8f9
Thanawat PHATARAWORAMET <thanawat.pha@live.ku.th>
2021-07-24 15:55:55 +0700

    edit webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,33 +9,7 @@ const TerserJSPlugin = require('terser-webpack-plugin');
 module.exports = {
     entry: {
       'layout/layout': './templates/layout/layout.js',
-      'web/home': './templates/web/home.js',
-      'layout/datatables': './templates/layout/datatables.js', // <-- add this line
-      'web/users': './templates/web/users.js', // <-- add this line
-      'web/customers': './templates/web/customers.js', // <-- add this line
-      'web/vendors': './templates/web/vendors.js', // <-- add this line
-      'web/spare_parts': './templates/web/spare_parts.js', // <-- add this line
-      'web/products': './templates/web/products.js', // <-- add this line
-      'web/categories': './templates/web/categories.js', // <-- add this line
-      'web/models': './templates/web/models.js', // <-- add this line
-      'web/machines': './templates/web/machines.js', // <-- add this line
-      'web/tool_layout_standards': './templates/web/tool_layout_standards.js', // <-- add this line
-      'web/tool_layout_standard_details': './templates/web/tool_layout_standard_details.js', // <-- add this line
-      'web/spare_part_issues': './templates/web/spare_part_issues.js', // <-- add this line
-      'web/spare_part_issue_details': './templates/web/spare_part_issue_details.js', // <-- add this line
-      'web/edit_spare_part_issue_detail': './templates/web/edit_spare_part_issue_detail.js', // <-- add this line
-      'web/transfer_stores': './templates/web/transfer_stores.js', // <-- add this line
-      'web/transfer_store_details': './templates/web/transfer_store_details.js', // <-- add this line
-      'web/report_spare_part_usage': './templates/web/report_spare_part_usage.js', // <-- add this line
-      'web/report_spare_part_movement': './templates/web/report_spare_part_movement.js', // <-- add this line
-      'web/down_prices': './templates/web/down_prices.js', // <-- add this line
-      'web/down_price_details': './templates/web/down_price_details.js', // <-- add this line    
-      'web/master_cost': './templates/web/master_cost.js', // <-- add this line
-      'web/count_stocks': './templates/web/count_stocks.js', // <-- add this line
-      'web/count_stock_details': './templates/web/count_stock_details.js', // <-- add this line
-      // here you can add more entries for each page or global assets like jQuery and bootstrap
-      // 'layout/layout': './templates/layout/layout.js'
-      
+      'web/home': './templates/web/home.js',      
     },
     output: {
       path: path.resolve(__dirname, 'public/assets'),
