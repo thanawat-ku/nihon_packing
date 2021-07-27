e8964c0ba91bee5f1f984c1aae8f8eca3804428a
Nontakorn Konkain <nonami798@gmail.com>
2021-07-27 17:26:14 +0700

    add delete lot

--- config/routes.php
+++ config/routes.php
@@ -31,5 +31,6 @@ return function (App $app) {
     $app->get('/lots', \App\Action\Web\LotAction::class)->add(UserAuthMiddleware::class);
     $app->post('/add_lot', \App\Action\Web\LotAddAction::class)->add(UserAuthMiddleware::class);
     $app->post('/edit_lot', \App\Action\Web\LotEditAction::class)->add(UserAuthMiddleware::class);
+    $app->post('/delete_lot', \App\Action\Web\LotEditAction::class)->add(UserAuthMiddleware::class);
     
 };
