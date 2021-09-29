d7c4551c9d0c614277399e652f407c8466a1742b
kumA0taku <varinpimda@gmail.com>
2021-09-29 21:26:03 +0700

    ms sql server v2

--- src/Domain/Product/Repository/ProductRepository.php
+++ src/Domain/Product/Repository/ProductRepository.php
@@ -3,6 +3,7 @@
 namespace App\Domain\Product\Repository;
 
 use App\Factory\QueryFactory;
+use App\Factory\QueryFactory2;
 use DomainException;
 use Cake\Chronos\Chronos;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -10,11 +11,13 @@ use Symfony\Component\HttpFoundation\Session\Session;
 final class ProductRepository
 {
     private $queryFactory;
+    private $queryFactory2;
     private $session;
 
-    public function __construct(Session $session,QueryFactory $queryFactory)
+    public function __construct(Session $session,QueryFactory $queryFactory,QueryFactory2 $queryFactory2)
     {
         $this->queryFactory = $queryFactory;
+        $this->queryFactory2 = $queryFactory2;
         $this->session=$session;
     }
     public function insertProduct(array $row): int
@@ -40,7 +43,7 @@ final class ProductRepository
     }
     
 
-    public function findProducts(array $params): array
+    public function findProduct(array $params): array
     {
         $query = $this->queryFactory->newSelect('products');
         
@@ -51,6 +54,23 @@ final class ProductRepository
                 'part_name',
                 'std_pack',
                 'std_box',
+            ]
+        );
+
+        return $query->execute()->fetchAll('assoc') ?: [];
+    }
+
+    public function findProducts(array $params): array
+    {
+        $query = $this->queryFactory2->newSelect('product');
+        
+        $query->select(
+            [
+                'id'=>'ProductID',
+                'part_code'=>'PartCode',
+                'part_name'=>'PartName',
+                'std_pack'=>'PackingStd',
+                'std_box'=>'BoxStd',
             ]
         );
 
