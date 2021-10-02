76df731659dfd15b632854d453297996a32f6f15
Thanawat Phattaraworamet <thanawat.pha@ku.th>
2021-10-02 18:06:31 +0700

    change print use domain method

--- src/Action/Web/LotPrintAction.php
+++ src/Action/Web/LotPrintAction.php
@@ -9,6 +9,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use App\Domain\Lot\Service\LotFinder;
 use App\Domain\Lot\Service\LotUpdater;
 use App\Domain\Label\Service\LabelUpdater;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
  * Action.
@@ -19,12 +20,15 @@ final class LotPrintAction
     private $updater;
     private $finder;
     private $labelUpdater;
+    private $session;
     public function __construct(
+        Session $session,
         Responder $responder,
         LotUpdater $updater,
         LotFinder $finder,
         LabelUpdater $labelUpdater
     ) {
+        $this->session = $session;
         $this->responder = $responder;
         $this->updater = $updater;
         $this->finder = $finder;
@@ -47,48 +51,54 @@ final class LotPrintAction
         $lots = $this->finder->findLots($params);
 
         if ($lots[0]['status'] == "CREATED") {
-            // $merge_status = (string)($data['merge_status'] ?? '');
-            $lot_id = (int)($lots[0]['id'] ?? 1);
-            $real_qty = (int)$realQty ?? 1;
-            $std_pack = (int)$lots[0]['std_pack'] ?? 1;
-            $num_packs = ceil($real_qty / $std_pack);
-            $num_full_packs = floor($real_qty / $std_pack);
+        //     // $merge_status = (string)($data['merge_status'] ?? '');
+        //     $lot_id = (int)($lots[0]['id'] ?? 1);
+        //     $real_qty = (int)$realQty ?? 1;
+        //     $std_pack = (int)$lots[0]['std_pack'] ?? 1;
+        //     $num_packs = ceil($real_qty / $std_pack);
+        //     $num_full_packs = floor($real_qty / $std_pack);
 
-            $labels = [];
-            for ($i = 0; $i < $num_full_packs; $i++) {
-                $data1['lot_id']  = $lot_id;
-                $data1['label_no'] = "X" . str_pad($i, 11, "0", STR_PAD_LEFT);
-                $data1['label_type'] = "FULLY";
-                $data1['quantity'] = $std_pack;
-                $data1['status'] = "CREATED";
-                $id = $this->labelUpdater->insertLabel($data1);
-                $data1['label_no'] = "P" . str_pad($id, 11, "0", STR_PAD_LEFT);
-                $this->labelUpdater->updateLabel($id, $data1);
-                $label['id'] = $id;
-                $label['label_no'] = $data1['label_no'];
-                $label['label_type'] = $data1['label_type'];
-                $label['quantity'] = $data1['quantity'];
-                array_push($labels, $label);
-            }
-            if ($num_full_packs != $num_packs) {
-                $data1['id']  = $lot_id;
-                $data1['label_no'] = "X" . str_pad($i, 11, "0", STR_PAD_LEFT);
-                $data1['label_type'] = "NONFULLY";
-                $data1['quantity'] = $real_qty - ($num_full_packs * $std_pack);
-                $data1['status'] = "CREATED";
-                $id = $this->labelUpdater->insertLabel($data1);
-                $data1['label_no'] = "P" . str_pad($id, 11, "0", STR_PAD_LEFT);
-                $this->labelUpdater->updateLabel($id, $data1);
-                $label['id'] = $id;
-                $label['label_no'] = $data1['label_no'];
-                $label['label_type'] = $data1['label_type'];
-                $label['quantity'] = $data1['quantity'];
-                array_push($labels, $label);
-            }
-            $dataReal['real_qty'] = $realQty;
-            $this->updater->printLot($lotId, $dataReal);
-
-            
+        //     $labels = [];
+        //     for ($i = 0; $i < $num_full_packs; $i++) {
+        //         $data1['lot_id']  = $lot_id;
+        //         $data1['label_no'] = "X" . str_pad($i, 11, "0", STR_PAD_LEFT);
+        //         $data1['label_type'] = "FULLY";
+        //         $data1['quantity'] = $std_pack;
+        //         $data1['status'] = "CREATED";
+        //         $id = $this->labelUpdater->insertLabel($data1);
+        //         $data1['label_no'] = "P" . str_pad($id, 11, "0", STR_PAD_LEFT);
+        //         $this->labelUpdater->updateLabel($id, $data1);
+        //         $label['id'] = $id;
+        //         $label['label_no'] = $data1['label_no'];
+        //         $label['label_type'] = $data1['label_type'];
+        //         $label['quantity'] = $data1['quantity'];
+        //         array_push($labels, $label);
+        //     }
+        //     if ($num_full_packs != $num_packs) {
+        //         $data1['id']  = $lot_id;
+        //         $data1['label_no'] = "X" . str_pad($i, 11, "0", STR_PAD_LEFT);
+        //         $data1['label_type'] = "NONFULLY";
+        //         $data1['quantity'] = $real_qty - ($num_full_packs * $std_pack);
+        //         $data1['status'] = "CREATED";
+        //         $id = $this->labelUpdater->insertLabel($data1);
+        //         $data1['label_no'] = "P" . str_pad($id, 11, "0", STR_PAD_LEFT);
+        //         $this->labelUpdater->updateLabel($id, $data1);
+        //         $label['id'] = $id;
+        //         $label['label_no'] = $data1['label_no'];
+        //         $label['label_type'] = $data1['label_type'];
+        //         $label['quantity'] = $data1['quantity'];
+        //         array_push($labels, $label);
+        //     }
+        //     $dataReal['real_qty'] = $realQty;
+        //     $this->updater->printLot($lotId, $dataReal);
+            $data['user_id']=$this->session->get('user')["id"];
+            $data['lot_id']=$lotId;
+            $data['real_qty']=$realQty;
+            $data['std_pack']=$lots[0]['std_pack'];
+            $this->labelUpdater->genLabelNo($data);
+            $dataLot['user_id']=$this->session->get('user')["id"];
+            $dataLot['status']='PRINTED';
+            $this->updater->updateLot($lotId, $dataLot);
         }
 
 
