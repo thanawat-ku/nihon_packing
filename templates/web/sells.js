3523d111de83ef25a7e927063e47fe130e13585a
racha01 <70879490+racha01@users.noreply.github.com>
2021-12-06 21:06:40 +0700

    register sell

--- templates/web/sells.js
+++ templates/web/sells.js
@@ -4,28 +4,37 @@ $(function () {
         format: 'yyyy-mm-dd'
     });
 });
-function editSell(event){
+function editSell(event) {
     let sell = event.currentTarget.name;
     console.log(sell);
     var obj = JSON.parse(sell);
     $("#editSellID").val(obj.id);
     $("#editSellNo").text(obj.sell_no);
     $("#editSellStatus").val(obj.sell_status);
-    
+
 }
 
-function deleteSell(event){
+function deleteSell(event) {
     let sell = event.currentTarget.name;
     console.log(sell);
     var obj = JSON.parse(sell);
     $("#deleteSellID").val(obj.id);
     $("#deleteSellNo").text(obj.sell_no);
-    
+
+}
+
+function registerTag(event) {
+    let sell = event.currentTarget.name;
+    console.log(sell);
+    var obj = JSON.parse(sell);
+    $("#regisSellID").val(obj.id);
+    $("#regisSellNo").text(obj.sell_no);
+
 }
 
 $(document).on(
     "click",
-    "#editBt, #deleteBt",
+    "#editBt, #deleteBt, #registerBt",
     (event) => {
         let id = event.currentTarget.id;
         switch (id) {
@@ -35,6 +44,8 @@ $(document).on(
             case "deleteBt":
                 deleteSell(event);
                 break;
+            case "registerBt":
+                registerTag(event);
             default:
                 console.log("no any events click");
         }
