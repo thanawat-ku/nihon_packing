6eeee6406dd859ec9d439ed3d5b5b2e4a588c3b0
Thanawat PHATARAWORAMET <thanawat.pha@live.ku.th>
2021-12-25 13:11:44 +0700

    margin on label

--- zpl/label.py
+++ zpl/label.py
@@ -1,31 +1,34 @@
 from zebra import Zebra
+from time import sleep
 z = Zebra()
 z.getqueues()
 z.setqueue('ZDesigner GT800 (EPL)')
-label="""
-^XA
-^FO10,15^A0,15,15^FDPartNo:^FS
-^FO10,35^A0,20,20^FD2CM-F3113-00-00-80 7751 9422^FS
-^FO10,55^GB320,3,3^FS
-^FO10,65^A0,15,15^FDPartName:^FS
-^FO10,85^A0,20,20^FDCS STOPPER LEVER SHAFT B94(JQ76)^FS
-^FO10,105^GB320,3,3^FS
-^FO10,115^A0,15,15^FDLotNo:^FS
-^FO10,135^A0,20,20^FD21503K20R10B^FS
-^FO330,65^GB270,3,3^FS
-^FO345,15^A0,15,15^FDQuantity:^FS
-^FO410,15^A0,60,60^FD20,000^FS
-^FO330,15^GB5,180,3^FS
-^FO345,75^BY2^BC,60,,,,A^FDPK1234567890^FS
-^FO330,160^GB270,3,3^FS
-^FO10,155^GB320,3,3^FS
-^FO10,165^A0,15,15^FDVisual By:^FS
-^FO10,180^A0,20,20^FDthanawat^FS
-^FO160,155^GB3,40,3^FS
-^FO170,165^A0,15,15^FDPack By:^FS
-^FO350,170^A0,25,25^FDNIHON SEIKI THAI LTD.^FS
-^XZ
-"""
-#z.autosense()
-#z.setup( direct_thermal=None, label_height=None, label_width=609 )
-z.output(label)
+for i in range(10):
+    label="""
+    ^XA
+    ^FO125,15^A0,15,15^FDPartNo:^FS
+    ^FO125,35^A0,20,20^FD2CM-F3113-00-00-80 7751 9422^FS
+    ^FO125,55^GB320,3,3^FS
+    ^FO125,65^A0,15,15^FDPartName:^FS
+    ^FO125,85^A0,20,20^FDCS STOPPER LEVER SHAFT B94(JQ76)^FS
+    ^FO125,105^GB320,3,3^FS
+    ^FO125,115^A0,15,15^FDLotNo:^FS
+    ^FO125,135^A0,20,20^FD21503K20R10B^FS
+    ^FO445,65^GB270,3,3^FS
+    ^FO460,15^A0,15,15^FDQuantity:^FS
+    ^FO535,15^A0,60,60^FD20,000^FS
+    ^FO445,15^GB5,180,3^FS
+    ^FO460,75^BY2^BC,60,,,,A^FDP"""+str(i+21).zfill(11)+"""^FS
+    ^FO445,160^GB270,3,3^FS
+    ^FO125,155^GB320,3,3^FS
+    ^FO125,165^A0,15,15^FDVisual By:^FS
+    ^FO125,180^A0,20,20^FDthanawat^FS
+    ^FO275,155^GB3,40,3^FS
+    ^FO285,165^A0,15,15^FDPack By:^FS
+    ^FO465,170^A0,25,25^FDNIHON SEIKI THAI LTD.^FS
+    ^XZ
+    """
+    #z.autosense()
+    #z.setup( direct_thermal=None, label_height=None, label_width=609 )
+    z.output(label)
+    sleep(1)
