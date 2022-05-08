fbcad63162f915ea847722bb4b29dff131033c66
racha01 <70879490+racha01@users.noreply.github.com>
2022-05-08 15:30:14 +0700

    edit isset product is add pack

--- src/Action/Api/PackAddAction.php
+++ src/Action/Api/PackAddAction.php
@@ -48,53 +48,52 @@ final class PackAddAction
 
         $data = (array)$request->getParsedBody();
 
-        if (isset($data['user_id'])) {
+        if (isset($data['user_id']) && $data['product_id']) {
             $user_id = $data['user_id'];
-        }
+            $prodcutID = $data['product_id'];
 
-        $prodcutID = $data['product_id'];
+            $checkCreatePack = true;
 
-        $checkCreatePack = true;
-
-        $rtPack = $this->finder->findPacks($data);
-        for ($i = 0; $i < count($rtPack); $i++) {
-            if ($rtPack[$i]['product_id'] == $prodcutID && ($rtPack[$i]['pack_status'] == "CREATED" || $rtPack[$i]['pack_status'] == "SLECTING_CPO" || $rtPack[$i]['pack_status'] == "SLECTED_CPO" || $rtPack[$i]['pack_status'] == "SLECTING_LABEL")) {
-                $checkCreatePack = ["Pack Not Empty"];
-                $rtdata['message'] = "Get Lot Defect Successful";
-                $rtdata['error'] = true;
-                $rtdata['packs'] = $checkCreatePack;
-                $checkCreatePack = false;
-                break;
-            }
-        }
-
-        if ($checkCreatePack == true) {
-            $id = $this->updater->insertPackApi($data, $user_id);
-            $data['id'] = $id;
-            $rtdata['message'] = "Get Lot Defect Successful";
-            $rtdata['error'] = false;
-            $rtdata['packs'] = $this->finder->findPacks($data);
-
-            $params['pack_id'] = $id;
-            $params['product_id'] = $prodcutID;
-            $cpodata = $this->findCpoItem->findCpoItem($params);
-            $uuid = uniqid();
-
-            $cpoitemcheck = $this->findTempQuery->findTempQueryCheck($params);
-
-            if (!$cpoitemcheck) {
-                foreach ($cpodata as $cpo) {
-                    $param_cpo['uuid'] = $uuid;
-                    $param_cpo['cpo_item_id'] = $cpo['CpoItemID'];
-                    $param_cpo['product_id'] = $cpo['ProductID'];
-                    $param_cpo['quantity'] = $cpo['Quantity'];
-                    $param_cpo['packing_qty'] = $cpo['PackingQty'];
-                    $param_cpo['due_date'] = $cpo['DueDate'];
-                    $this->updateTempQuery->insertTempQuery($param_cpo);
+            $rtPack = $this->finder->findPacks($data);
+            for ($i = 0; $i < count($rtPack); $i++) {
+                if ($rtPack[$i]['product_id'] == $prodcutID && ($rtPack[$i]['pack_status'] == "CREATED" || $rtPack[$i]['pack_status'] == "SLECTING_CPO" || $rtPack[$i]['pack_status'] == "SLECTED_CPO" || $rtPack[$i]['pack_status'] == "SLECTING_LABEL")) {
+                    $checkCreatePack = ["Pack Not Empty"];
+                    $rtdata['message'] = "Get Lot Defect Successful";
+                    $rtdata['error'] = true;
+                    $rtdata['packs'] = $checkCreatePack;
+                    $checkCreatePack = false;
+                    break;
                 }
             }
-        }
 
-        return $this->responder->withJson($response, $rtdata);
+            if ($checkCreatePack == true) {
+                $id = $this->updater->insertPackApi($data, $user_id);
+                $data['id'] = $id;
+                $rtdata['message'] = "Get Lot Defect Successful";
+                $rtdata['error'] = false;
+                $rtdata['packs'] = $this->finder->findPacks($data);
+
+                $params['pack_id'] = $id;
+                $params['product_id'] = $prodcutID;
+                $cpodata = $this->findCpoItem->findCpoItem($params);
+                $uuid = uniqid();
+
+                $cpoitemcheck = $this->findTempQuery->findTempQueryCheck($params);
+
+                if (!$cpoitemcheck) {
+                    foreach ($cpodata as $cpo) {
+                        $param_cpo['uuid'] = $uuid;
+                        $param_cpo['cpo_item_id'] = $cpo['CpoItemID'];
+                        $param_cpo['product_id'] = $cpo['ProductID'];
+                        $param_cpo['quantity'] = $cpo['Quantity'];
+                        $param_cpo['packing_qty'] = $cpo['PackingQty'];
+                        $param_cpo['due_date'] = $cpo['DueDate'];
+                        $this->updateTempQuery->insertTempQuery($param_cpo);
+                    }
+                }
+            }
+
+            return $this->responder->withJson($response, $rtdata);
+        }
     }
 }
